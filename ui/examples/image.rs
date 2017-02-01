aa331519baa1748d122ee44a8c15682abb081081
Marc Schettke <schettke@ms2.info>
2017-02-01 17:37:59 +0100

    Add example for drawing an image

---
+++ ui/examples/image.rs
@@ -0,0 +1,136 @@
+//! An example control gallery: a port of the same `ui` example.
+
+extern crate ui;
+
+use ui::{BoxControl, Button, Checkbox, ColorButton, Combobox, DateTimePicker, Entry};
+use ui::{FontButton, Group, InitOptions, Label, Menu, MenuItem, ProgressBar, RadioButtons};
+use ui::{Separator, Slider, Spinbox, Tab, Window};
+use ui::{Image, Area, AreaHandler, AreaKeyEvent, AreaDrawParams};
+
+struct ImageAreaHandler {
+    data: Vec<u32>
+}
+
+impl AreaHandler for ImageAreaHandler {
+    fn draw(&mut self, area: &Area, area_draw_params: &AreaDrawParams) {
+        let img = Image::new(100, 100);
+        img.load_pixmap(0, 0, 100, 100, &self.data);
+        area_draw_params.context.draw_image(0.0, 0.0, &img);
+    }
+
+    fn key_event(&mut self, _area: &Area, _area_key_event: &AreaKeyEvent) -> bool {
+        println!("key");
+        true
+    }
+}
+
+fn run() {
+    let menu = Menu::new("File");
+    menu.append_item("Open").on_clicked(Box::new(open_clicked));
+    menu.append_item("Save").on_clicked(Box::new(save_clicked));
+
+    let menu = Menu::new("Edit");
+    menu.append_check_item("Checkable Item");
+    menu.append_separator();
+    let item = menu.append_item("Disabled Item");
+    item.disable();
+    menu.append_preferences_item();
+
+    let menu = Menu::new("Help");
+    menu.append_item("Help");
+    menu.append_about_item();
+
+    let mainwin = Window::new("ui Control Gallery", 640, 480, true);
+    mainwin.set_margined(true);
+    mainwin.on_closing(Box::new(|_| {
+        ui::quit();
+        false
+    }));
+
+    let vbox = BoxControl::new_vertical();
+    vbox.set_padded(true);
+    mainwin.set_child(vbox.clone().into());
+
+    let hbox = BoxControl::new_horizontal();
+    hbox.set_padded(true);
+    vbox.append(hbox.clone().into(), true);
+
+    let group = Group::new("Basic Controls");
+    group.set_margined(true);
+    hbox.append(group.clone().into(), false);
+
+    let inner = BoxControl::new_vertical();
+    inner.set_padded(true);
+    group.set_child(inner.clone().into());
+
+    inner.append(Button::new("Button").into(), false);
+    inner.append(Checkbox::new("Checkbox").into(), false);
+    let entry = Entry::new();
+    entry.set_text("Entry");
+    inner.append(entry.into(), false);
+    inner.append(Label::new("Label").into(), false);
+    inner.append(Separator::new_horizontal().into(), false);
+
+    inner.append(DateTimePicker::new_date_picker().into(), false);
+    inner.append(DateTimePicker::new_time_picker().into(), false);
+    inner.append(DateTimePicker::new_date_time_picker().into(), false);
+
+    inner.append(FontButton::new().into(), false);
+    inner.append(ColorButton::new().into(), false);
+
+    let inner2 = BoxControl::new_vertical();
+    inner2.set_padded(true);
+    hbox.append(inner2.clone().into(), true);
+
+    let group = Group::new("Numbers");
+    group.set_margined(true);
+    inner2.append(group.clone().into(), false);
+
+    let inner = BoxControl::new_vertical();
+    inner.set_padded(true);
+    group.set_child(inner.clone().into());
+
+    let spinbox = Spinbox::new(0, 100);
+    spinbox.on_changed(Box::new(|spinbox| update(spinbox.value())));
+    inner.append(spinbox.into(), false);
+
+    let slider = Slider::new(0, 100);
+    slider.on_changed(Box::new(|slider| update(slider.value())));
+    inner.append(slider.into(), false);
+
+    let progress_bar = ProgressBar::new();
+    inner.append(progress_bar.into(), false);
+
+    let area = Area::new(Box::new(ImageAreaHandler {data: vec![0xff123456;100*100]}));
+    inner2.append(area.into(),false);
+
+    mainwin.show();
+    ui::main();
+}
+
+pub fn main() {
+    ui::init(InitOptions).unwrap();
+    run();
+    ui::uninit();
+}
+
+fn open_clicked(_: &MenuItem, mainwin: &Window) {
+    match ui::open_file(mainwin) {
+        Some(filename) => ui::msg_box(mainwin, "File selected", &*filename),
+        None => ui::msg_box_error(mainwin, "No file selected", "Don't be alarmed!"),
+    }
+}
+
+fn save_clicked(_: &MenuItem, mainwin: &Window) {
+    match ui::open_file(mainwin) {
+        Some(filename) => {
+            ui::msg_box(mainwin, "File selected (don't worry, it's still there)", &*filename)
+        }
+        None => ui::msg_box_error(mainwin, "No file selected", "Don't be alarmed!"),
+    }
+}
+
+fn update(_: i64) {
+    // TODO(pcwalton)
+}
+
