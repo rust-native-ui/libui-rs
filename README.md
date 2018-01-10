e121ccf4bdeea7b49d4c6aee5d05ce39ebe9f920
Leo Tindall <lfstindall@gmail.com>
2018-01-09 17:05:28 -0800

    Make libui, ui-sys, and ui examples build properly
    This involved actually invoking cmake to build the library rather than
    just running make on the build directory, so cmake is now a dependency.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 These are work-in-progress Rust bindings to the minimalistic native UI library [libui][libui].
 
+They require GNU Make and CMake to build `libui` itself.
+
 Author: Patrick Walton (@pcwalton)
 
 [libui]: https://github.com/andlabs/libui
--- ui-sys/build.rs
+++ ui-sys/build.rs
@@ -1,23 +1,32 @@
 extern crate make_cmd;
+extern crate cmake;
 
 use std::env;
 use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    // Update the submodule with libui if needed
     if !Path::new("libui/.git").exists() {
         Command::new("git").args(&["submodule", "update", "--init"]).status().unwrap();
     }
 
+    // Run cmake to build the project's makefiles
+    let dst = cmake::Config::new("libui")
+                             .build_target("")
+                             .build();
+    let dst = format!("{}/build", dst.display());
+
+    // Run make to build the actual library
     let out_dir = env::var("OUT_DIR").unwrap();
     let outdir_argument = format!("OUTDIR={}", out_dir);
     let objdir_argument = format!("OBJDIR={}/obj", out_dir);
-    make_cmd::gnu_make().args(&["-C", "libui", &*outdir_argument, &*objdir_argument])
+    make_cmd::gnu_make().args(&["-C", &dst, &*outdir_argument, &*objdir_argument])
                         .status()
                         .unwrap();
-    Command::new("cp").args(&["-r", "libui/out/", &*out_dir]).status().unwrap();
+    //Command::new("cp").args(&["-r", "libui/out/", &*out_dir]).status().unwrap();
 
+    println!("cargo:rustc-link-search=native={}/out/", dst);
     println!("cargo:rustc-link-lib=dylib=ui");
-    println!("cargo:rustc-link-search=native={}", out_dir);
 }
 
