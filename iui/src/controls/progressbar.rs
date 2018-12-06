a429c23ba1aa4f4cb5632c02f8cd40dd0d42352d
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-12-05 21:36:41 -0600

    Add doc comments, rename ProgressBarStyle to ProgressBarValue

--- iui/src/controls/progressbar.rs
+++ iui/src/controls/progressbar.rs
@@ -3,50 +3,70 @@ use std::mem;
 use ui::UI;
 use ui_sys::{self, uiControl, uiProgressBar};
 
+/// An enum representing the value of a `ProgressBar`
+pub enum ProgressBarValue {
+    /// Represents a set, consistent percentage of the bar to be filled
+    Determinate(u32),
+    /// Represents an indeterminate value of the progress bar, useful
+    /// if you don't know how much of the task being represented is completed
+    Indeterminate,
+}
+
 define_control! {
-  /// A bar that fills up with a set percentage, used to show completion of a task
+  /// A bar that fills up with a set percentage, used to show completion of a
+  ///
+  /// # Values
+  /// A `ProgressBar` can be either determinate or indeterminate. See [`ProgressBarValue`]
+  /// for an explanation of the differences.
+  ///
+  /// [`ProgressBarValue`]: enum.ProgressBarValue.html
   rust_type: ProgressBar,
   sys_type: uiProgressBar,
 }
 
-pub enum ProgressBarStyle {
-    Determinate(u32),
-    Indeterminate,
-}
-
 impl ProgressBar {
+    /// Create a new progress bar with a value of 0
     pub fn new() -> ProgressBar {
         unsafe { ProgressBar::from_raw(ui_sys::uiNewProgressBar()) }
     }
 
+    /// Create a new indeterminate progress bar
     pub fn indeterminate() -> ProgressBar {
         let mut pb = ProgressBar::new();
-        pb.set_value(ProgressBarStyle::Indeterminate);
+        pb.set_indeterminate();
         pb
     }
 
-    pub fn set_value(&mut self, value: ProgressBarStyle) {
+    /// Set the value of the progress bar to a determinate value
+    pub fn set_determinate(&mut self, value: u32) {
+        self.set_value(ProgressBarValue::Determinate(value));
+    }
+
+    /// Set the value of the progress bar to be indeterminate
+    pub fn set_indeterminate(&mut self) {
+        self.set_value(ProgressBarValue::Indeterminate);
+    }
+
+    /// Set the value of the progress bar
+    pub fn set_value(&mut self, value: ProgressBarValue) {
         let sys_value = match value {
-            ProgressBarStyle::Determinate(value) => value as i32,
-            ProgressBarStyle::Indeterminate => -1,
+            ProgressBarValue::Determinate(value) => value as i32,
+            ProgressBarValue::Indeterminate => -1,
         };
         unsafe { ui_sys::uiProgressBarSetValue(self.uiProgressBar, sys_value) }
     }
 
-    pub fn set_determinate(&mut self, value: u32) {
-        self.set_value(ProgressBarStyle::Determinate(value));
-    }
-
-    pub fn value(&self) -> ProgressBarStyle {
+    /// Get the value of the progress bar
+    pub fn value(&self) -> ProgressBarValue {
         let sys_value = unsafe { ui_sys::uiProgressBarValue(self.uiProgressBar) };
         if sys_value.is_negative() {
             assert!(
                 sys_value == -1,
                 "if ProgressBar value is negative it can only be -1"
             );
-            ProgressBarStyle::Indeterminate
+            ProgressBarValue::Indeterminate
         } else {
-            ProgressBarStyle::Determinate(sys_value as u32)
+            ProgressBarValue::Determinate(sys_value as u32)
         }
     }
 }
