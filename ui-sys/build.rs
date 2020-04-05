836d844658b03f14e306835f387e4387b049124c
Sean Cross <sean@xobs.io>
2020-04-05 12:45:40 +0800

    ui-sys: remove cmake dependency for windows
    Remove the need to call `cmake` on Windows.  This is the first commit in
    a series that allows for statically building programs without external
    dependencies.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ui-sys/build.rs
+++ ui-sys/build.rs
@@ -1,7 +1,7 @@
-extern crate cmake;
 extern crate bindgen;
+extern crate cc;
+extern crate embed_resource;
 
-use cmake::Config;
 use bindgen::Builder as BindgenBuilder;
 
 use std::env;
@@ -43,38 +43,151 @@ fn main() {
         .expect("Couldn't write bindings");
 
     // Deterimine build platform
-    let target = env::var("TARGET").unwrap();
-    let msvc = target.contains("msvc");
-    let apple = target.contains("apple");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_triple = env::var("TARGET").unwrap();
+    let msvc = target_triple.contains("msvc");
+    let apple = target_triple.contains("apple");
 
     // Build libui if needed. Otherwise, assume it's in lib/
-    let mut dst;
     if cfg!(feature = "build") {
-        let mut cfg = Config::new("libui");
-        cfg.build_target("").profile("release");
-        if apple {
-            cfg.cxxflag("--stdlib=libc++");
-        }
-        dst = cfg.build();
+        let mut base_config = cc::Build::new();
+        let src_base = env::var("SRC_BASE").unwrap_or("libui".to_string());
 
-        let mut postfix = Path::new("build").join("out");
-        if msvc {
-            postfix = postfix.join("Release");
+        base_config.include(format!("{}{}", src_base, "/common"));
+
+        base_config.file(format!("{}{}", src_base, "/common/attribute.c"));
+        base_config.file(format!("{}{}", src_base, "/common/attrlist.c"));
+        base_config.file(format!("{}{}", src_base, "/common/attrstr.c"));
+        base_config.file(format!("{}{}", src_base, "/common/areaevents.c"));
+        base_config.file(format!("{}{}", src_base, "/common/control.c"));
+        base_config.file(format!("{}{}", src_base, "/common/debug.c"));
+        base_config.file(format!("{}{}", src_base, "/common/matrix.c"));
+        base_config.file(format!("{}{}", src_base, "/common/opentype.c"));
+        base_config.file(format!("{}{}", src_base, "/common/shouldquit.c"));
+        base_config.file(format!("{}{}", src_base, "/common/tablemodel.c"));
+        base_config.file(format!("{}{}", src_base, "/common/tablevalue.c"));
+        base_config.file(format!("{}{}", src_base, "/common/userbugs.c"));
+        base_config.file(format!("{}{}", src_base, "/common/utf.c"));
+
+        if target_os == "windows" {
+            base_config.cpp(true);
+            base_config.include(format!("{}{}", src_base, "/windows"));
+
+            base_config.file(format!("{}{}", src_base, "/windows/alloc.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/area.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/areadraw.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/areaevents.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/areascroll.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/areautil.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/attrstr.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/box.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/button.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/checkbox.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/colorbutton.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/colordialog.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/combobox.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/container.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/control.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/d2dscratch.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/datetimepicker.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/debug.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/draw.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/drawmatrix.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/drawpath.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/drawtext.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/dwrite.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/editablecombo.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/entry.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/events.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/fontbutton.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/fontdialog.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/fontmatch.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/form.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/graphemes.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/grid.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/group.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/image.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/init.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/label.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/main.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/menu.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/multilineentry.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/opentype.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/parent.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/progressbar.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/radiobuttons.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/separator.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/sizing.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/slider.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/spinbox.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/stddialogs.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/tab.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/table.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/tabledispinfo.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/tabledraw.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/tableediting.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/tablemetrics.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/tabpage.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/text.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/utf16.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/utilwin.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/window.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/winpublic.cpp"));
+            base_config.file(format!("{}{}", src_base, "/windows/winutil.cpp"));
+
+            // See https://github.com/nabijaczleweli/rust-embed-resource/issues/11
+            let target = env::var("TARGET").unwrap();
+            if let Some(tool) = cc::windows_registry::find_tool(target.as_str(), "cl.exe") {
+                for (key, value) in tool.env() {
+                    env::set_var(key, value);
+                }
+            }
+            embed_resource::compile(format!("{}{}", src_base, "/windows/resources.rc"));
+
+            link("user32", false);
+            link("kernel32", false);
+            link("gdi32", false);
+            link("comctl32", false);
+            link("uxtheme", false);
+            link("msimg32", false);
+            link("comdlg32", false);
+            link("d2d1", false);
+            link("dwrite", false);
+            link("ole32", false);
+            link("oleaut32", false);
+            link("oleacc", false);
+            link("uuid", false);
+            link("windowscodecs", false);
         }
-        dst = dst.join(&postfix);
+        base_config.compile("libui.a");
     } else {
-        dst = env::current_dir()
-            .expect("Unable to retrieve current directory location.");
+        // If we're not building the library, then assume it's pre-built and exists in `lib/`
+        let mut dst = env::current_dir().expect("Unable to retrieve current directory location.");
         dst.push("lib");
-    }
 
-    let libname;
-    if msvc {
-        libname = "libui";
+        let libname = if msvc { "libui" } else { "ui" };
+
+        println!("cargo:rustc-link-search=native={}", dst.display());
+        println!("cargo:rustc-link-lib={}", libname);
+    }
+}
+
+/// Tell cargo to link the given library, and optionally to bundle it in.
+pub fn link(name: &str, bundled: bool) {
+    let target = env::var("TARGET").unwrap();
+    let target: Vec<_> = target.split('-').collect();
+    if target.get(2) == Some(&"windows") {
+        println!("cargo:rustc-link-lib=dylib={}", name);
+        if bundled && target.get(3) == Some(&"gnu") {
+            let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+            println!("cargo:rustc-link-search=native={}/{}", dir, target[0]);
+        }
     } else {
-        libname = "ui";
+        println!("cargo:rustc-link-lib=dylib={}", name);
     }
+}
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib={}", libname);
+/// Add the given framework to the linker path
+pub fn link_framework(name: &str) {
+    println!("cargo:rustc-link-lib=framework={}", name);
 }
